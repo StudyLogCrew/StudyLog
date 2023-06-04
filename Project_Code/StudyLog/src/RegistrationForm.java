9805634d88f38300a87034b8eb5811e5897d4042
Sarantis Papachristofilou <saradis.pap7@gmail.com>
2023-06-04 18:37:03 +0300

    Φάκελοι src(κώδικας), lib(mysql connector)

---
+++ Project_Code/StudyLog/src/RegistrationForm.java
@@ -0,0 +1,10 @@
+public class RegistrationForm {
+    String onoma;
+    String epitheto;
+    String passward;
+    int age;
+    int ID;
+    public RegistrationForm(){
+        ID=0;
+    }
+}
---
+++ Project_Code/StudyLog/src/Teacher.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Teacher extends User{
+    int arithmosProtereotitas;
+    ArrayList <String> = new ArrayList <string>();
+    public Teacher(RegistrationForm dilosoulaTreloula){
+        super(dilosoulaTreloula,true);
+        arithmosProtereotitas=0;
+    }
+}
